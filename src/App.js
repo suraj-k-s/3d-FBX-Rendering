036feb2dcd69135a447f9886afbf5420af20fddf
Suraj K S <56590063+suraj-k-s@users.noreply.github.com>
2023-03-22 03:41:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ReactThreeFbxViewer from "react-three-fbx-for-pyt";
+
+let fbxUrl = require("./demo.fbx");
+
+export default class App extends Component {
+  onLoad(e) {
+    console.log(e);
+  }
+
+  onError(e) {
+    console.log(e);
+  }
+  render() {
+    let cameraPosition = {
+      x: 150,
+      y: 300,
+      z: 350,
+    };
+    return (
+      <div>
+        <ReactThreeFbxViewer
+          cameraPosition={cameraPosition}
+          url={fbxUrl}
+          onLoading={this.onLoad}
+          onError={this.onError}
+        />
+      </div>
+    );
+  }
+}
